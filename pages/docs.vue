6b5cfdc50ef8785c23d4f1a315614b5034b0a2da
elskater98 <45320338+elskater98@users.noreply.github.com>
2020-11-28 13:45:11 +0100

    getChanges done

--- pages/docs.vue
+++ pages/docs.vue
@@ -19,13 +19,11 @@
               </v-list-item>
             </v-card>
           </v-row>
-          <v-row>
-        {{this.$data}}
-          </v-row>
         </v-col>
         <v-col>
           <v-row>
             <v-slider
+              @change="getChanges"
               v-model="slider.val"
               :label="slider.label"
               :thumb-color="slider.color"
@@ -36,16 +34,19 @@
           </v-row>
           <v-row>
             <v-checkbox
+              @change="getChanges"
               v-model="checkbox1"
               :label="'Header'"
             ></v-checkbox>
             <v-checkbox style="margin-left: 10%"
                         v-model="checkbox2"
                         :label="'Subtitle'"
+                        @change="getChanges"
             ></v-checkbox>
           </v-row>
           <v-row>
             <v-textarea
+              @keyup="getChanges"
               filled
               name="input-7-4"
               label="Filled textarea"
@@ -77,9 +78,17 @@ export default {
     sendChange(){
 
     },
+    recoverData(){
 
+    },
     getChanges(){
-
+    return {checkbox1:this.$data.checkbox1,
+        checkbox2:this.$data.checkbox2,
+        slider: {label:'slider',val:this.$data.slider.val,color:'red'},
+        textarea1:this.$data.textarea1,
+        subtitle:this.$data.subtitle,
+        header:this.$data.header,
+        summary:this.$data.summary};
     }
 
   }
