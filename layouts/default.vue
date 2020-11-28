97811fc1bf5eaa08db5eda0cc3f75654b726b4ed
elskater98 <45320338+elskater98@users.noreply.github.com>
2020-11-28 12:35:09 +0100

    Init Collab page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,78 @@
 <template>
+  <v-app>
+    <div>
+      <v-toolbar dark>
+        <!--<v-toolbar-title>Nuxt Template</v-toolbar-title>-->
+        <v-btn to="/" depressed nuxt><v-icon medium>mdi-home</v-icon>Home</v-btn>
+        <v-btn to="/docs" depressed nuxt>My Collab</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn depressed @click="loginDialog=true">
+          <v-icon medium>mdi-account</v-icon>
+          Log in
+        </v-btn>
+      </v-toolbar>
+      <v-dialog persistent
+                max-width="600px"
+                v-model="loginDialog">
+        <v-card>
+          <form>
+            <v-card-title>
+              <span class="headline">Log in</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
 
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Email"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="loginDialog = false">Close
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="login">Save
+              </v-btn>
+            </v-card-actions>
+          </form>
+        </v-card>
+      </v-dialog>
+      <Nuxt/>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: "default"
+  name: "default",
+  data() {
+    return {email: "", password: "", loginDialog: false}
+  },
+  methods: {
+    login() {
+      //TODO: HTTP REQ
+      //TODO: CHECK RESPONSE
+      //TODO: SEND ERROR OR CLOSE DIALOG AND SAVE DATA.
+      this.loginDialog = false;
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
---
+++ pages/docs.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col></v-col>
+        <v-col>
+          <v-row>
+            <v-combobox
+              solo
+              v-model="comboboxSelect"
+              :items="comboboxItems"
+              outlined
+              dense>
+            </v-combobox>
+          </v-row>
+
+          <v-row>
+            <v-checkbox
+              v-model="checkbox1"
+              :label="'Checkbox1'"
+            ></v-checkbox>
+            <v-checkbox
+              v-model="checkbox2"
+              :label="'Checkbox2'"
+            ></v-checkbox>
+          </v-row>
+
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "docs",
+  data(){
+    return {
+      checkbox1:false,
+      checkbox2:false,
+      slider1:0,
+      textarea1:"",
+      comboboxSelect:[],
+      comboboxItems:['Red','Blue','Green']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
